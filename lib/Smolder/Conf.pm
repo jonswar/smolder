acbfd9ed462e1d3a3ca3f38fa5f51ca0c19012f6
Jonathan Swartz <swartz@pobox.com>
2009-08-04 15:51:04 -0400

    add User and Group; tidy

--- lib/Smolder/Conf.pm
+++ lib/Smolder/Conf.pm
@@ -11,8 +11,8 @@ use Smolder;
 sub _random_secret {
     my $length = int(rand(5) + 10);
     my $secret = '';
-    my @chars = ('a'..'z', 'A'..'Z', 0..9);
-    $secret .= $chars[int(rand($#chars + 1))] for(0..$length);
+    my @chars  = ('a' .. 'z', 'A' .. 'Z', 0 .. 9);
+    $secret .= $chars[int(rand($#chars + 1))] for (0 .. $length);
     return $secret;
 }
 
@@ -21,30 +21,32 @@ my %VALUES;
 BEGIN {
     my $share_dir;
     my $blib_share_dir = rel2abs(catdir(curdir, 'blib', 'lib', 'auto', 'share', 'dist', 'Smolder'));
-    if( -d $blib_share_dir ) {
+    if (-d $blib_share_dir) {
         $share_dir = $blib_share_dir;
     } else {
         $share_dir = dist_dir('Smolder');
     }
 
     %VALUES = (
-        Port                  => 8080,
-        HostName              => 'localhost.localdomain',
+        AutoRedirectToProject => 0,
+        AutoRefreshReports    => 0,
+        DataDir               => catdir(File::HomeDir->my_data, '.smolder'),
+        ErrorsToScreen        => 0,
         FromAddress           => 'smolder@localhost.localdomain',
-        SMTPHost              => 'localhost.localdomain',
+        Group                 => undef,
+        HostName              => 'localhost.localdomain',
+        HtdocsDir             => catdir($share_dir, 'htdocs'),
         LogFile               => '',
         PidFile               => catdir($share_dir, 'data', 'smolder.pid'),
-        TemplateDir           => catdir($share_dir, 'templates'),
-        DataDir               => catdir(File::HomeDir->my_data, '.smolder'),
-        HtdocsDir             => catdir($share_dir, 'htdocs'),
+        Port                  => 8080,
+        ReportsPerPage        => 5,
+        SMTPHost              => 'localhost.localdomain',
         SQLDir                => catdir($share_dir, 'sql'),
         Secret                => _random_secret(),
-        AutoRefreshReports    => 0,
-        TruncateTestFilenames => 0,
-        ErrorsToScreen        => 0,
-        ReportsPerPage        => 5,
+        TemplateDir           => catdir($share_dir, 'templates'),
         TestSourceRoot        => undef,
-        AutoRedirectToProject => 0,
+        TruncateTestFilenames => 0,
+        User                  => undef,
     );
 }
 
@@ -99,7 +101,7 @@ Override the configuration defaults by providing named-value pairs:
 sub init {
     my ($class, %args) = @_;
     foreach my $key (keys %args) {
-        if( exists $VALUES{$key} ) {
+        if (exists $VALUES{$key}) {
             $VALUES{$key} = $args{$key};
         } else {
             croak "$key is not a valid Smolder config parameter!";
@@ -125,14 +127,16 @@ whitespace:
 
 sub init_from_file {
     my ($class, $file) = @_;
-    croak "Config file $file does not exist!" unless -e $file;
+    croak "Config file $file does not exist!"  unless -e $file;
     croak "Config file $file is not readable!" unless -r $file;
 
     open(my $FH, '<', $file) or croak "Could not open file $file for reading: $!";
     my %values;
-    while(my $line = <$FH>) {
+    while (my $line = <$FH>) {
+
         # only get lines that look like name-value pairs but not comments
-        if( $line !~ /\s*#/ && $line =~ /(\S+)\s+(\S+)/ ) {
+        if ($line !~ /\s*#/ && $line =~ /(\S+)\s+(\S+)/) {
+
             # strip off any quotes
             my ($key, $val) = ($1, $2);
             $val =~ s/^'(.*)'$/$1/;
@@ -158,7 +162,7 @@ Directive names are case-insensitive.
 
 sub get {
     my ($class, $key) = @_;
-    if( exists $VALUES{$key} ) {
+    if (exists $VALUES{$key}) {
         return $VALUES{$key};
     } else {
         croak "$key is not a valid Smolder config parameter!";
